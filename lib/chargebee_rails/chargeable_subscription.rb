6bec840063f1d55d1a148c39ba16509d975269a0
Sairam <sai.messy@gmail.com>
2016-03-31 12:55:07 +0530

    Change subscription update and cancellation actions

--- lib/chargebee_rails/chargeable_subscription.rb
+++ lib/chargebee_rails/chargeable_subscription.rb
@@ -1,26 +1,29 @@
 module ChargebeeRails
   module ChargeableSubscription
-    def change_plan(plan)
-      result = ChargeBee::Subscription.update(chargebee_id, { plan_id: plan.plan_id })
-      subscription = result.subscription
+    def change_plan(plan, end_of_term=false)
+      subscription = ChargeBee::Subscription.update(
+        chargebee_id, { plan_id: plan.plan_id, end_of_term: end_of_term }
+      ).subscription
       update(
         chargebee_plan: subscription.plan_id, 
         plan_id: plan.id,
         status: subscription.status
-      )
+      ) unless end_of_term
     end
 
-    def cancel
-      result = ChargeBee::Subscription.cancel(chargebee_id)
-      subscription = result.subscription
+    def cancel(end_of_term=false)
+      subscription = ChargeBee::Subscription.cancel(chargebee_id).subscription
       update(
         status: subscription.status
-      )
+      ) unless end_of_term
+    end
+
+    def stop_cancellation
+      ChargeBee::Subscription.remove_scheduled_cancellation(chargebee_id).subscription
     end
 
     def reactivate
-      result = ChargeBee::Subscription.reactivate(chargebee_id)
-      subscription = result.subscription
+      subscription = ChargeBee::Subscription.reactivate(chargebee_id).subscription
       update(
         status: subscription.status
       )
