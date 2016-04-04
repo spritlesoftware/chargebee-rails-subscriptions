31d9f4295ec1ffb25bfe0c99779d61a3e309cfc4
Sairam <sai.messy@gmail.com>
2016-04-04 14:35:21 +0530

    Add configuration for end of term subscription changes and inclusion of delayed charges for estimations

--- lib/chargebee_rails/chargeable_subscription.rb
+++ lib/chargebee_rails/chargeable_subscription.rb
@@ -1,18 +1,28 @@
 module ChargebeeRails
   module ChargeableSubscription
-    def change_plan(plan, end_of_term=false)
-      subscription = ChargeBee::Subscription.update(
-        chargebee_id, { plan_id: plan.plan_id, end_of_term: end_of_term }
-      ).subscription
-      update(
-        chargebee_plan: subscription.plan_id, 
-        plan_id: plan.id,
-        status: subscription.status
-      ) unless end_of_term
+
+    def self.included(base)
+      base.extend(ClassMethods)
     end
 
-    def cancel(end_of_term=false)
-      subscription = ChargeBee::Subscription.cancel(chargebee_id).subscription
+    def as_chargebee_subscription
+      ChargeBee::Subscription.retrieve(chargebee_id).subscription
+    end
+
+    def change_plan(plan, options={})
+      options[:plan_id] = plan.plan_id
+      options[:end_of_term] ||= ChargebeeRails.configuration.end_of_term
+      subscription = ChargeBee::Subscription.update(chargebee_id, options).subscription
+      update(
+        chargebee_plan: subscription.plan_id,
+        plan_id: plan.id,
+        status: subscription.status
+      ) unless options[:end_of_term]
+    end
+
+    def cancel(options={})
+      options[:end_of_term] ||= ChargebeeRails.configuration.end_of_term
+      subscription = ChargeBee::Subscription.cancel(chargebee_id, options).subscription
       update(
         status: subscription.status
       ) unless end_of_term
@@ -28,5 +38,24 @@ module ChargebeeRails
         status: subscription.status
       )
     end
+
+    def renewal_estimate(options={})
+      options[:include_delayed_charges] ||= ChargebeeRails.configuration.include_delayed_charges[:renewal_estimate]
+      ChargeBee::Estimate.renewal_estimate(chargebee_id, options).estimate
+    end
+
+    module ClassMethods 
+      
+      def estimate(estimation_params)
+        ::ChargeBee::Estimate.create_subscription(estimation_params).estimate
+      end
+
+      def estimate_changes(estimation_params)
+        estimation_params[:include_delayed_charges] ||= ChargebeeRails.configuration.include_delayed_charges[:changes_estimate]
+        ::ChargeBee::Estimate.update_subscription(estimation_params).estimate
+      end
+
+    end
+
   end
 end
--- lib/chargebee_rails/configuration.rb
+++ lib/chargebee_rails/configuration.rb
@@ -14,9 +14,13 @@ module ChargebeeRails
 
   class Configuration
     attr_accessor :default_plan_id
+    attr_accessor :end_of_term
+    attr_accessor :include_delayed_charges
 
     def initialize
       @default_plan_id = nil
+      @end_of_term = false
+      @include_delayed_charges = {changes_estimate: false, renewal_estimate: true}
     end
   end
 end
