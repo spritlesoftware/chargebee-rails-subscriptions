f4e026d319393f6f85984d2b38f227218626cbe3
Sairam <sai.messy@gmail.com>
2016-04-07 14:39:36 +0530

    Add comments to subscription methods and update subscription builder

--- lib/chargebee_rails/chargeable_subscription.rb
+++ lib/chargebee_rails/chargeable_subscription.rb
@@ -5,9 +5,12 @@ module ChargebeeRails
       base.extend(ClassMethods)
     end
 
+    # Get the Chargebee equivalent subscription for the corresponding
+    # active record subscription
     def as_chargebee_subscription
       ChargeBee::Subscription.retrieve(chargebee_id).subscription
     end
+
 
     def change_plan(plan, options={})
       options[:plan_id] = plan.plan_id
@@ -20,6 +23,9 @@ module ChargebeeRails
       ) unless options[:end_of_term]
     end
 
+    # Cancel a subscription - it will be scheduled for cancellation at term end
+    # when end_of_term is passed as true. If no options are passed the 
+    # default configured value for end_of_term is taken
     def cancel(options={})
       options[:end_of_term] ||= ChargebeeRails.configuration.end_of_term
       subscription = ChargeBee::Subscription.cancel(chargebee_id, options).subscription
@@ -28,10 +34,12 @@ module ChargebeeRails
       ) unless end_of_term
     end
 
+    # Stop a scheduled cancellation of a subscription
     def stop_cancellation
       ChargeBee::Subscription.remove_scheduled_cancellation(chargebee_id).subscription
     end
 
+    # Reactivate a cancelled subscription
     def reactivate
       subscription = ChargeBee::Subscription.reactivate(chargebee_id).subscription
       update(
@@ -39,17 +47,21 @@ module ChargebeeRails
       )
     end
 
+    # Estimates the subscription's renewal  
     def renewal_estimate(options={})
       options[:include_delayed_charges] ||= ChargebeeRails.configuration.include_delayed_charges[:renewal_estimate]
       ChargeBee::Estimate.renewal_estimate(chargebee_id, options).estimate
     end
 
-    module ClassMethods 
+    module ClassMethods
       
+      # Estimates the cost of subscribing to a new subscription
       def estimate(estimation_params)
         ::ChargeBee::Estimate.create_subscription(estimation_params).estimate
       end
 
+      # Estimates the cost of changes to an existing subscription
+      # estimates the upgrade/downgrade or other changes
       def estimate_changes(estimation_params)
         estimation_params[:include_delayed_charges] ||= ChargebeeRails.configuration.include_delayed_charges[:changes_estimate]
         ::ChargeBee::Estimate.update_subscription(estimation_params).estimate
--- lib/chargebee_rails/subscription_builder.rb
+++ lib/chargebee_rails/subscription_builder.rb
@@ -9,11 +9,12 @@ module ChargebeeRails
     # Here we will create a subscription in Chargebee,
     # update the resulting subscription details for the customer in the 
     # active record database and finally return the 
-    # active record subscription object
+    # active record subscription
     def create
-      @result = create_chargebee_subscription
-      @customer.update(chargebee_id: @result.customer.id)
-      create_local_subscription
+      build_subscription_payload
+      create_chargebee_subscription
+      update_customer_chargebee_id
+      create_active_record_subscription
       update_card
       @subscription
     end
@@ -22,12 +23,17 @@ module ChargebeeRails
 
     # Create a subscription in Chargebee with the passed options payload
     def create_chargebee_subscription
-      ChargeBee::Subscription.create(subscription_payload)
+      @result = ChargeBee::Subscription.create(@options)
+    end
+
+    # Update the chargebee customer id for the subscription owner
+    def update_customer_chargebee_id
+      @customer.update(chargebee_id: @result.customer.id)
     end
 
     # Create an active record subscription of the chargebee subscription object 
     # for the customer
-    def create_local_subscription
+    def create_active_record_subscription
       @chargebee_subscription = @result.subscription
       @subscription = @customer.create_subscription({
         chargebee_id: @chargebee_subscription.id,
@@ -51,8 +57,8 @@ module ChargebeeRails
     # Check for the default plan if one is not passed in the options payload
     # raise plan not configured error incase plan is not passed and a default
     # plan is not set in the ChargebeeRails configuration. 
-    # Raise plan not found if the plan passed is not found in active record  
-    def subscription_payload
+    # Raise plan not found if the plan passed is not found in active record
+    def build_subscription_payload
       @options[:plan_id] ||= ChargebeeRails.configuration.default_plan_id
       raise PlanError.new.plan_not_configureed unless @options[:plan_id]
       @plan = Plan.find_by(plan_id: @options[:plan_id])
