8eb1c768e519a79757a255b99c3f5781d522c0b2
Sairam <sai.messy@gmail.com>
2016-04-26 17:18:28 +0530

    Handle remote subscription updates in webhook event handler

--- lib/chargebee_rails/event_handler.rb
+++ lib/chargebee_rails/event_handler.rb
@@ -5,6 +5,7 @@ module ChargebeeRails
     # corresponding event type handler for the event
     def handle(chargebee_event)
       @chargebee_event = chargebee_event
+      handle_remote_chargebee_subscription_updates if check_subscription_update
       send(event.event_type)
     end
 
@@ -25,35 +26,17 @@ module ChargebeeRails
 
     def subscription_started; end
 
-    def subscription_trial_end_reminder; end
+    def subscription_trial_ending; end
 
     def subscription_activated; end
 
-    def subscription_changed
-      subscription_event = event.content.subscription
-      subscription = ::Subscription.find_by(chargebee_id: subscription_event.id)
-      subscription.update(
-        chargebee_plan: subscription_event.plan_id,
-        plan: ::Plan.find_by(plan_id: subscription_event.plan_id),
-        status: subscription_event.status,
-        has_scheduled_changes: subscription_event.has_scheduled_changes
-      ) if subscription.has_scheduled_changes
-    end
+    def subscription_changed; end
 
     def subscription_cancellation_scheduled; end
 
     def subscription_cancellation_reminder; end
 
-    def subscription_cancelled
-      subscription_event = event.content.subscription
-      subscription = ::Subscription.find_by(chargebee_id: subscription_event.id)
-      subscription.update(
-        chargebee_plan: subscription_event.plan_id,
-        plan: ::Plan.find_by(plan_id: subscription_event.plan_id),
-        status: subscription_event.status,
-        has_scheduled_changes: subscription_event.has_scheduled_changes
-      ) if subscription.has_scheduled_changes
-    end
+    def subscription_cancelled; end
 
     def subscription_reactivated; end
 
@@ -106,6 +89,28 @@ module ChargebeeRails
     def card_expired; end
 
     def card_deleted; end
+
+    private
+
+    def check_subscription_update
+      remote_subscription_events.include? event.event_type
+    end
+
+    def handle_remote_chargebee_subscription_updates
+      puts "inside handle subscription", event
+      subscription_event = event.content.subscription
+      subscription = ::Subscription.find_by(chargebee_id: subscription_event.id)
+      subscription.update(
+        chargebee_plan: subscription_event.plan_id,
+        plan: ::Plan.find_by(plan_id: subscription_event.plan_id),
+        status: subscription_event.status,
+        has_scheduled_changes: subscription_event.has_scheduled_changes
+      ) if subscription.try(:has_scheduled_changes)
+    end
+
+    def remote_subscription_events
+      %w( subscription_activated subscription_changed subscription_renewed subscription_cancelled )
+    end
 
   end
 end
