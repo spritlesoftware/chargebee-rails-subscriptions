80acdabc9ddac011a675a92f16b09c01bc30b039
Philip Bjorge <philipbjorge@gmail.com>
2018-06-01 22:30:40 +0700

    Update event_sync_log_migration.rb

--- lib/generators/chargebee_rails/templates/event_sync_log_migration.rb
+++ lib/generators/chargebee_rails/templates/event_sync_log_migration.rb
@@ -1,4 +1,10 @@
-class CreateEventSyncLog < ActiveRecord::Migration
+migration_superclass = if ActiveRecord::VERSION::MAJOR >= 5
+  ActiveRecord::Migration[4.2]
+else
+  ActiveRecord::Migration
+end
+
+class CreateEventSyncLog < migration_superclass
   def change
     create_table :event_sync_logs do |t|
 
